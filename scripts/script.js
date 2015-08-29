a6684d60a3aa7a02c766e43407081061e03679cf
Stefan <spiazza@opera.com>
2015-08-29 12:41:11 +0100

    Firefox prefixes

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ for (var i = 0; i < columns.length; i++) {
 
 function toggleWidth(column) {
     column.style.webkitTransitionDuration = '500ms';
+    column.style.transitionDuration = '500ms';
 
     if (!column.expanded) {
         column.expanded = true;
@@ -21,14 +22,17 @@ function toggleWidth(column) {
 function attachListeners(element) {
     element.addEventListener('webkitTransitionEnd', function () {
         element.style.webkitTransitionDuration = '';
+        element.style.transitionDuration = '';
     });
 
     element.addEventListener('orientationchange', function () {
         element.style.webkitTransitionDuration = '';
+        element.style.transitionDuration = '';
     });
 
     element.addEventListener('resize', function () {
         element.style.webkitTransitionDuration = '';
+        element.style.transitionDuration = '';
     });
 }
 
